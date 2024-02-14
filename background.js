be0a2c4c16731d6058e5e32c1cf361b768429fac
Adyahrudrah <102347712+Adyahrudrah@users.noreply.github.com>
2024-02-14 15:41:52 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.openNewTab) {
-        // Open the new tab with the bookmarks.html page.
-        chrome.tabs.create({ url: chrome.runtime.getURL('bookmarks.html') });
+chrome.tabs.onCreated.addListener(function(tab) {
+    if ((tab.url.trim().toLowerCase() === 'edge://newtab/')){
+    chrome.tabs.update(tab.id, { url: chrome.runtime.getURL("bookmarks.html") });
     }
-});
+  });
