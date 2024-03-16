64f37d318437ad7a6c012f922995dc1559f219b0
Adyahrudrah <102347712+Adyahrudrah@users.noreply.github.com>
2024-03-16 20:47:08 +0530

    //

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.tabs.onCreated.addListener(function(tab) {
-    if ((tab.url.trim().toLowerCase() === 'edge://newtab/')){
-    chrome.tabs.update(tab.id, { url: chrome.runtime.getURL("bookmarks.html") });
-    }
-  });
